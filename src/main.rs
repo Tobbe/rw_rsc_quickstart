6b5e0ffd99728c766f6f6d06496e58c95f6974a1
Tobbe Lundberg <tobbe@tlundberg.com>
2024-07-17 22:16:49 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,81 @@
+use serde_json::Map;
+use serde_json::Value;
+use std::fs;
+use std::io::Cursor;
+use std::path::Path;
+use std::path::PathBuf;
+
+fn main() {
+    if !Path::new("template").exists() {
+        let url = "https://github.com/redwoodjs/redwood/archive/refs/heads/main.zip";
+        let resp = reqwest::blocking::get(url).expect("request failed");
+        let archive = resp.bytes().expect("body invalid");
+
+        let target_dir = get_tempdir();
+        println!("Extracting into {}", target_dir.to_string_lossy());
+
+        // The third parameter allows you to strip away toplevel directories.
+        // If `archive` contained a single directory, its contents would be extracted instead.
+        zip_extract::extract(Cursor::new(archive), &target_dir, true)
+            .expect("Failed to extract zip");
+
+        let from = target_dir
+            .join("__fixtures__")
+            .join("test-project-rsc-kitchen-sink");
+
+        fs::rename(from, "template").expect("Failed to rename");
+
+        fs::remove_dir_all(target_dir).expect("Failed to remove temp dir");
+    }
+
+    let latest_rw_canary = "8.0.0-canary.904";
+
+    let package_jsons = glob::glob("template/**/package.json").expect("Failed to glob");
+
+    for entry in package_jsons {
+        let path = entry.expect("Failed to get path");
+
+        println!(
+            "Updating {} to use latest RW canary version",
+            path.to_string_lossy()
+        );
+
+        let contents = fs::read_to_string(&path).expect("Failed to read file");
+
+        let mut json: serde_json::Value =
+            serde_json::from_str(&contents).expect("Failed to parse json");
+
+        let dependencies = match json["dependencies"].as_object_mut() {
+            Some(deps) => deps,
+            None => &mut Map::new(),
+        };
+
+        for (name, value) in dependencies.iter_mut() {
+            if name.starts_with("@redwoodjs/") {
+                *value = Value::String(latest_rw_canary.into());
+            }
+        }
+
+        let dev_dependencies = match json["devDependencies"].as_object_mut() {
+            Some(dev_deps) => dev_deps,
+            None => &mut Map::new(),
+        };
+
+        for (name, value) in dev_dependencies.iter_mut() {
+            if name.starts_with("@redwoodjs/") {
+                *value = Value::String(latest_rw_canary.into());
+            }
+        }
+
+        fs::write(&path, json.to_string()).expect("Failed to write file");
+    }
+}
+
+fn get_tempdir() -> PathBuf {
+    tempfile::Builder::new()
+        .prefix("rwjs-rsc-quickstart-")
+        .rand_bytes(12)
+        .tempdir()
+        .unwrap()
+        .into_path()
+}
